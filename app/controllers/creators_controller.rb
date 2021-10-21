bea37e0e7151a51259e9319a6ea7351ed3b5539d
Victoria Tiller <vijotiller@gmail.com>
2021-10-21 14:20:23 -0400

    shows creator name on creator page

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,5 +1,15 @@
 class CreatorsController < ApplicationController
+    before_action :find_creator, only: [:show]
     def index 
         @creators = Creator.all
+    end
+
+    def show
+    end
+
+    private
+
+    def find_creator
+        @creator = Creator.find(params[:id])
     end
 end
--- app/controllers/musicals_controller.rb
+++ app/controllers/musicals_controller.rb
@@ -1,6 +1,17 @@
 class MusicalsController < ApplicationController
-
+    before_action :find_musical, only: [:show]
     def index 
         @musicals = Musical.all 
     end
+
+    def show
+        # @musical = Musical.find(params[:id])
+    end
+
+    private
+
+    def find_musical
+        @musical = Musical.find(params[:id])
+    end
+
 end
