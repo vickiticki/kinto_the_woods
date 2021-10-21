f7685683340878db174d39a0c5110d23584d558c
Victoria Tiller <vijotiller@gmail.com>
2021-10-21 17:08:55 -0400

    new and edit musical form started

--- app/controllers/musicals_controller.rb
+++ app/controllers/musicals_controller.rb
@@ -1,7 +1,7 @@
 class MusicalsController < ApplicationController
-    before_action :find_musical, only: [:show]
+    before_action :find_musical, only: [:show, :edit, :update]
     def index 
-        @musicals = Musical.all 
+        @musicals = Musical.all
     end
 
     def show
@@ -10,7 +10,38 @@ class MusicalsController < ApplicationController
         @songs = @musical.songs
     end
 
+    def new
+        @musical = Musical.new
+      end
+    
+      def create 
+        @musical = Musical.new(musical_params)
+        if @musical.save
+          redirect_to @musical
+        else
+          render 'new'
+        end
+      end
+      
+      def edit
+        @musical = Musical.find(params[:id])
+      end
+    
+      def update
+        @musical.update(musical_params)
+        redirect_to musical_path(@musical)
+      end
+    
+    #   def destroy
+    #     @movie.destroy
+    #     redirect_to movies_path
+    #   end
+
     private
+
+    def musical_params
+        params.require(:musical).permit(:title, :synopsis, :release_year)
+    end
 
     def find_musical
         @musical = Musical.find(params[:id])
