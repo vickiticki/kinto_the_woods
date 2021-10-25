a04ffd674fb4987376a0d406559c32efc64afb3d
Victoria Tiller <vijotiller@gmail.com>
2021-10-25 13:55:35 -0400

    validates user name and email

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   # get '/musicals' => 'musicals#index'
-
+  
   # get '/creators' => 'creators#index'
-
+  
   resources :musicals, :creators, :books, :songs
+  resources :users
 
   # get '/books/new' => 'books#new'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_153933) do
+ActiveRecord::Schema.define(version: 2021_10_25_174341) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -72,6 +72,13 @@ ActiveRecord::Schema.define(version: 2021_10_25_153933) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["musical_id"], name: "index_songs_on_musical_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
