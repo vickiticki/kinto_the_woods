14ce0b6bf87018cf53af32b0d3a7f9e9aaba1fa7
Victoria Tiller <vijotiller@gmail.com>
2021-11-23 08:55:21 -0500

    destroying users also deletes their lists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,7 @@ class UsersController < ApplicationController
 
   # DELETE /users/1 or /users/1.json
   def destroy
+    List.where(id: @user.lists).delete_all
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
